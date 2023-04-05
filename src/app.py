65a2327c20330fc8d3908f5df9ab8fae3dced1c5
Bjarne <83133360+BjarneIO@users.noreply.github.com>
2023-04-05 15:39:03 +0200

    newcss and template

--- src/app.py
+++ src/app.py
@@ -3,11 +3,15 @@ from dotenv import load_dotenv
 from flask import Flask
 from routes.root import root
 from routes.login import login
+from routes.register import register
 
 app = Flask(__name__, template_folder='./templates', static_folder='./static')
+app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
+
 
 app.register_blueprint(root, url_prefix='/')
 app.register_blueprint(login, url_prefix='/login')
+app.register_blueprint(register, url_prefix='/register')
 
 if __name__ == '__main__':
     load_dotenv()
